ee2ae6a3b6bbeaeea0fd602e47ab6aa40957baa9
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-05-18 03:05:08 +0530

    clear new branch

--- Lib/Utils/GuiPops.py
+++
@@ -1,2 +0,0 @@
-def showStatus(parent, message, interval=5000):
-    parent.ui.statusbar.showMessage(message, interval)
--- Lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from .UI import ActionCenter, home, customPushButton
-from .FileHandling import FileOpener
-from .Utils.GuiPops import *
-from .Models import *
-from .Process import Thread,ThreadObjects,Classes
